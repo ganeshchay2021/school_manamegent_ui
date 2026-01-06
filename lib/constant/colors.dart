2d4499b1544aa90d7c3d2ac926c1e823911c11ff
Ganesh Chaudhary <ganeshchay2021@gmail.com>
2026-01-06 15:24:12 +0545

    App Colors Added

---
+++ lib/constant/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const kBackground = Color(0xFFf9f9fc);
+const textColor = Color(0xFF263064);
+const secondTextColor = Color(0xFF3c31c5);
+const kheaderColor = Color(0xFFd5e7ff);
+const kCardColor = Color(0xFFf9f9fc);
